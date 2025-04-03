bbd54b7f0bb3dbee2b01e154bf4b75e279b3523b
Christopher Besch <mail@chris-besch.com>
2025-04-03 10:22:29 +0200

    feat: linearly interpolate lens inputs

--- python_src/main.py
+++ python_src/main.py
@@ -17,19 +17,15 @@ class MagneticField:
         self.field_max_loc = field_max_loc
 
 
-class LensInput:
+class LensParameters:
     def __init__(
         self,
-        output_ps_file: str,
-        output_png_file: str,
         sampling_points: int,
         magnetic_fields: List[MagneticField],
         optical_axis_length: float,
         acc_voltage: int,
     ):
         assert len(magnetic_fields) in [1, 2]
-        self.output_ps_file = output_ps_file
-        self.output_png_file = output_png_file
         self.sampling_points = sampling_points
         self.magnetic_fields = magnetic_fields
         # in mm
@@ -37,22 +33,85 @@ class LensInput:
         # in V
         self.acc_voltage = acc_voltage
 
+    def to_ndarray(self) -> np.ndarray:
+        if len(self.magnetic_fields) == 1:
+            return np.array([
+                self.sampling_points,
+                self.magnetic_fields[0].field_strength,
+                self.magnetic_fields[0].lateral_a,
+                self.magnetic_fields[0].field_max_loc,
+                self.optical_axis_length,
+                self.acc_voltage,
+            ])
+        elif len(self.magnetic_fields) == 2:
+            return np.array([
+                self.sampling_points,
+                self.magnetic_fields[0].field_strength,
+                self.magnetic_fields[0].lateral_a,
+                self.magnetic_fields[0].field_max_loc,
+                self.magnetic_fields[1].field_strength,
+                self.magnetic_fields[1].lateral_a,
+                self.magnetic_fields[1].field_max_loc,
+                self.optical_axis_length,
+                self.acc_voltage,
+            ])
+        else:
+            assert False
 
-def create_lens_input_str(lens_input: LensInput) -> str:
-    output = f"{lens_input.output_ps_file}\n"
-    output += f"{lens_input.sampling_points}\n"
-    output += f"{len(lens_input.magnetic_fields)}\n"
-    for magnetic_field in lens_input.magnetic_fields:
-        output += f"{magnetic_field.field_strength:.1f}\n"
-        output += f"{magnetic_field.lateral_a:.1f}\n"
-        output += f"{magnetic_field.field_max_loc:.1f}\n"
-    output += f"{lens_input.optical_axis_length:.1f}\n"
-    output += f"{lens_input.acc_voltage}\n"
-    return output
+    @classmethod
+    def from_ndarray(cls, vec: np.ndarray):
+        if len(vec) == 6:
+            return cls(
+                sampling_points=vec[0],
+                magnetic_fields=[
+                    MagneticField(
+                        field_strength=vec[1], lateral_a=vec[2], field_max_loc=vec[3]
+                    )
+                ],
+                optical_axis_length=vec[4],
+                acc_voltage=vec[5],
+            )
+        elif len(vec) == 9:
+            return cls(
+                sampling_points=vec[0],
+                magnetic_fields=[
+                    MagneticField(
+                        field_strength=vec[1], lateral_a=vec[2], field_max_loc=vec[3]
+                    ),
+                    MagneticField(
+                        field_strength=vec[4], lateral_a=vec[5], field_max_loc=vec[6]
+                    ),
+                ],
+                optical_axis_length=vec[7],
+                acc_voltage=vec[8],
+            )
+        else:
+            assert False
+
+
+class LensInput:
+    def __init__(
+        self, output_ps_file: str, output_png_file: str, lens_parameters: LensParameters
+    ):
+        self.output_ps_file = output_ps_file
+        self.output_png_file = output_png_file
+        self.lens_parameters = lens_parameters
+
+    def to_input_str(self) -> str:
+        output = f"{self.output_ps_file}\n"
+        output += f"{int(self.lens_parameters.sampling_points)}\n"
+        output += f"{len(self.lens_parameters.magnetic_fields)}\n"
+        for magnetic_field in self.lens_parameters.magnetic_fields:
+            output += f"{magnetic_field.field_strength:.1f}\n"
+            output += f"{magnetic_field.lateral_a:.1f}\n"
+            output += f"{magnetic_field.field_max_loc:.1f}\n"
+        output += f"{self.lens_parameters.optical_axis_length:.1f}\n"
+        output += f"{int(self.lens_parameters.acc_voltage)}\n"
+        return output
 
 
 def execute_lenses(inputs: List[LensInput]):
-    input_strs = list(map(create_lens_input_str, inputs))
+    input_strs = list(map(lambda i: i.to_input_str(), inputs))
     lens_procs: List[subprocess.Popen[bytes]] = []
     for input, input_str in zip(inputs, input_strs):
         print(f"running lens for {input.output_ps_file}")
@@ -104,22 +163,52 @@ def execute_lenses(inputs: List[LensInput]):
             sys.exit(1)
 
 
+def create_lens_frames(
+    out_dir: str, first_frame: LensParameters, last_frame: LensParameters, frames: int
+):
+    t_vals = np.linspace(0, 1, frames)
+    lens_parameters_vec_list = (
+        1 - t_vals[:, None]
+    ) * first_frame.to_ndarray() + t_vals[:, None] * last_frame.to_ndarray()
+    lens_parameters_list = [
+        LensParameters.from_ndarray(lens_parameters_vec)
+        for lens_parameters_vec in lens_parameters_vec_list
+    ]
+    lens_inputs = [
+        LensInput(
+            output_ps_file=f"{out_dir}/{n:03d}.ps",
+            output_png_file=f"{out_dir}/{n:03d}.png",
+            lens_parameters=lens_parameters,
+        )
+        for n, lens_parameters in enumerate(lens_parameters_list)
+    ]
+    execute_lenses(lens_inputs)
+
+
 def main():
     print("CTEMsoft lens")
     OUT_DIR = "out"
     os.makedirs(OUT_DIR, exist_ok=True)
-    execute_lenses([
-        LensInput(
-            output_ps_file=f"{OUT_DIR}/001.ps",
-            output_png_file=f"{OUT_DIR}/001.png",
+    create_lens_frames(
+        OUT_DIR,
+        first_frame=LensParameters(
             sampling_points=1000,
             magnetic_fields=[
-                MagneticField(field_strength=2, lateral_a=1, field_max_loc=0)
+                MagneticField(field_strength=1, lateral_a=1, field_max_loc=0)
             ],
             optical_axis_length=10,
             acc_voltage=100000,
-        )
-    ])
+        ),
+        last_frame=LensParameters(
+            sampling_points=1000,
+            magnetic_fields=[
+                MagneticField(field_strength=3, lateral_a=1, field_max_loc=0)
+            ],
+            optical_axis_length=10,
+            acc_voltage=100000,
+        ),
+        frames=3,
+    )
     print("All done")
     print("Have a very safe and productive day.")
 
